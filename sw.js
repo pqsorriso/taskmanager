45c285e8f81274717de899e6132076cf0ba6a679
PqSorriso <gustavo003nogueira@gmail.com>
2026-05-06 09:15:26 -0300

    🔄 Cache v8

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * sw.js — Service Worker para PWA
  * Versão com cache atualizado automaticamente
  */
-const CACHE_VERSION = 'v7';
+const CACHE_VERSION = 'v8';
 const CACHE_NAME = 'fceux-task-' + CACHE_VERSION;
 const ASSETS = [
   './',
